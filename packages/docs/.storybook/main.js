a2de08618d45c9e58f2f936f4052a465a0715013
MateusCecchin <mateusfernandescn@gmail.com>
2023-05-25 23:04:20 -0400

    fix: add subpath ao storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,5 +16,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType == "PRODUCTION") {
+      config.base = "/ignite-ui";
+    }
+    return config;
+  },
 };
 export default config;

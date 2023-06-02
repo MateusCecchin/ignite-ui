ee77a95e0ef75517039fdfcdb779d415db84b336
MateusCecchin <mateusfernandescn@gmail.com>
2023-06-01 23:32:25 -0400

    refactor: config base vite

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType == "PRODUCTION") {
-      config.base = "/goat-ui";
+      config.base = "/goat-ui/";
     }
     return config;
   },
